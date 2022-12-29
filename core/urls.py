cd21abc50a06470889270eaa452b0d001a4e267e
Igor Ruzhilov <iruzhilov@gmail.com>
2022-12-29 16:52:50 +0600

    Fix in core/urls.py file

--- core/urls.py
+++ core/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('course/', include('course.urls'
+    path('course/', include('course.urls'))
 ]
