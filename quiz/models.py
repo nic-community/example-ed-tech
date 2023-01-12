dcfbc9103ebfd627aadf59c352634136063e3237
Yun-Veronica <veronica@yun.kz>
2023-01-12 18:16:52 +0600

    Added student dield and changed relationships for language field

--- quiz/models.py
+++ quiz/models.py
@@ -1,16 +1,21 @@
 from django.db import models
 from course.models import Course
 from course.models import Language
+
+
+from django.contrib.auth.models import User
+
+
 # from lesson.models import Lesson
 
 
 # Create your models here.
 class FinalQuiz(models.Model):
-    course = models.OneToOneField(to=Course, on_delete=models.CASCADE,
-                                  verbose_name='Course id')  # зависит от course_id model
-    language = models.OneToOneField(to=Language, on_delete=models.CASCADE,
-                                    verbose_name='Language')  # язык курса совпадает с языком теста
-    total_mark = models.FloatField()
+    student = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Student") # студент к которому  привязывается конкретная оценка
+
+    course = models.OneToOneField(to=Course,on_delete=models.CASCADE,  verbose_name='Course id')  # зависит от course_id model
+    language = models.ManyToManyField(to=Language, verbose_name='Language')  # язык курса совпадает с языком теста
+    total_mark = models.FloatField() #оценка за тест для студента, а не в целом максимальная за тест
 
     def __str__(self):
         return f'Final mark for Quiz to Course {self.course.title} - {self.total_mark}'
@@ -19,17 +24,21 @@ class FinalQuiz(models.Model):
 class LessonQuiz(models.Model):
     # т. к. зависит от модели Lesson,то пока комментарием
     # lesson = models.OneToOneField(to=Lesson, on_delete=models.CASCADE,
-                                  # verbose_name='Lesson id')  # зависит от Lesson model
-    language = models.OneToOneField(to=Language, on_delete=models.CASCADE,
-                                    verbose_name='Language')  # язык курса совпадает с языком теста
+    #                               verbose_name='Lesson id')  # зависит от Lesson model
+
+    language = models.ManyToManyField(to=Language, verbose_name='Language')  # язык курса совпадает с языком теста
+
+    student = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Student") # студент, к которому привязывается конкретная оценка
+
     total_mark = models.FloatField()
+
     def __str__(self):
         return f'Final mark for Quiz to Lesson {self.lesson.name} - {self.total_mark}'
 
 
 class FinalQuizQuestion(models.Model):
     related_quiz = models.OneToOneField(FinalQuiz, on_delete=models.CASCADE,
-                                verbose_name='Quiz id')  # quiz, к которому применяется
+                                        verbose_name='Quiz id')  # quiz, к которому применяется
     text = models.CharField(max_length=100, verbose_name='Question text')  # текст вопроса
     mark = models.FloatField(verbose_name='Mark for question')  # кол-во баллов за правильный ответ на вопрос
 
@@ -39,7 +48,7 @@ class FinalQuizQuestion(models.Model):
 
 class LessonQuizQuestion(models.Model):
     related_quiz = models.OneToOneField(LessonQuiz, on_delete=models.CASCADE,
-                                verbose_name='Quiz id')  # quiz, к которому применяется
+                                        verbose_name='Quiz id')  # quiz, к которому применяется
     text = models.CharField(max_length=100, verbose_name='Question text')  # текст вопроса
     mark = models.FloatField(verbose_name='Mark for question')  # кол-во баллов за правильный ответ на вопрос
 
@@ -49,7 +58,7 @@ class LessonQuizQuestion(models.Model):
 
 class FinalQuizAnswer(models.Model):
     related_question = models.OneToOneField(FinalQuizQuestion,
-                                    on_delete=models.CASCADE)  # id того вопроса, к которому применяется
+                                            on_delete=models.CASCADE)  # id того вопроса, к которому применяется
     content = models.CharField(max_length=100)  # сам ответ
 
     def __str__(self):
@@ -58,7 +67,7 @@ class FinalQuizAnswer(models.Model):
 
 class LessonQuizAnswer(models.Model):
     related_question = models.OneToOneField(LessonQuizQuestion,
-                                    on_delete=models.CASCADE)  # id того вопроса, к которому применяется
+                                            on_delete=models.CASCADE)  # id того вопроса, к которому применяется
     content = models.CharField(max_length=100)  # сам ответ
 
     def __str__(self):
