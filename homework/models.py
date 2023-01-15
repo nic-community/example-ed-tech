e953cc73ac0ccfca622bac0507913afe363e3ceb
Igor Ruzhilov <iruzhilov@gmail.com>
2023-01-15 17:13:52 +0600

    Create User model

--- homework/models.py
+++ homework/models.py
@@ -1,7 +1,7 @@
 import uuid
 from django.db import models
-from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
+from user.models import User
 
 
 class HomeworkTaskModel(models.Model):
--- user/models.py
+++ user/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, UserManager
 from django.db import models
+from course.models import Course
+
 
 # Create your models here.
+class User(AbstractUser, PermissionsMixin):
+    email = models.EmailField(max_length=256, unique=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name="Курс ученика", blank=True, null=True)
+    date_of_birth = models.DateTimeField(blank=True, null=True)
+
+    # ENUM Roles
+    class Roles(models.IntegerChoices):
+        STUDENT = 1
+        TEACHER = 2
+        ADMIN = 3
+
+    role = models.IntegerField(choices=Roles.choices, default=Roles.STUDENT)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+
+    # LOGIN DATA
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
