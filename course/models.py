7c6d8ff3401ac84e26320d68e69c6d318de1bbcd
Igor Ruzhilov <iruzhilov@gmail.com>
2023-02-18 12:56:11 +0600

    Remove unnecessary comments in 'models.py' of 'course' app

--- course/models.py
+++ course/models.py
@@ -4,7 +4,6 @@ from django.db import models
 # Create your models here.
 
 class Language(models.Model):
-    # Название языка. К примеру: "Арабский"
     title = models.CharField(max_length=100)
 
     description = models.TextField(blank=True)
@@ -21,11 +20,8 @@ class Language(models.Model):
 
 
 class Lecturer(models.Model):
-    # Имя учителя
     first_name = models.CharField(max_length=50)
-    # Фамилия учителя
     last_name = models.CharField(max_length=50)
-    # Номер телефона учителя в формате: +72225554432
     phone_number = models.CharField(max_length=15, blank=True)
     email = models.CharField(max_length=256, blank=True)
 
@@ -43,18 +39,14 @@ class Lecturer(models.Model):
 
 
 class Course(models.Model):
-    # Название курса. К примеру: "Python Access to Web Data"
     title = models.TextField()
     # Партнёр курса. К примеру: "University of Michigan"
     partner = models.CharField(max_length=200)
     # Затрагиваемый стек. К примеру: "Python (Django)"
     topic = models.CharField(max_length=100)
-    # Наличие сертификата от Coursera (да/нет)
     has_certificate = models.BooleanField()
-    # Приблизительное время для прохождения курса в часах
     approximate_time_to_complete = models.IntegerField(default=0)
     rating = models.FloatField(default=0)
-    # Количество оставленных отзывов/оценок
     ratings_number = models.IntegerField(default=0)
 
     # ------ FOREIGN KEYS ------
