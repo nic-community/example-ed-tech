3d0e081f45236a12ab796ab74f9b5a33f84f5756
D-Diyazzz <diyazzz.dev@gmail.com>
2023-02-18 15:40:45 +0600

    Add repositories for homework models

--- homework/views/task_views.py
+++ homework/views/task_views.py
@@ -5,12 +5,13 @@ from rest_framework import status
 
 from ..serializers import *
 from ..models import *
+from ..repositories import HomeworkTaskRepository
 
 
 class TaskViewSet(viewsets.ViewSet):
     
     def list(self, request):
-        tasks = HomeworkTaskModel.objects.all()
+        tasks = HomeworkTaskRepository.get_all()
         serializer = HomeworkTaskResponseSerializer(tasks, many=True)
         return Response(serializer.data)
        
@@ -19,28 +20,28 @@ class TaskViewSet(viewsets.ViewSet):
         serializer = HomeworkTaskRequestSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            task = HomeworkTaskModel.objects.filter(pk=serializer.data["id"])
+            task = HomeworkTaskRepository.filter(pk=serializer.data["id"])
             return Response(task.values()[0], status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def retrieve(self, request, pk=None):
-        queryset = HomeworkTaskModel.objects.all()
+        queryset = HomeworkTaskRepository.get_all()
         task = get_object_or_404(queryset, pk=pk)
         serializer = HomeworkTaskResponseSerializer(task)
         return Response(serializer.data)
 
     def update(self, request, pk=None):
-        queryset = HomeworkTaskModel.objects.all()
+        queryset = HomeworkTaskRepository.get_all()
         task = get_object_or_404(queryset, pk=pk)
         serializer = HomeworkTaskRequestSerializer(task, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            task = HomeworkTaskModel.objects.filter(pk=serializer.data["id"])
+            task = HomeworkTaskRepository.filter(pk=serializer.data["id"])
             return Response(task.values()[0], status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, pk=None):
-        queryset = HomeworkTaskModel.objects.all()
+        queryset = HomeworkTaskRepository.get_all()
         task = get_object_or_404(queryset, pk=pk)
         task.delete()
         return Response("Homework task deleted", status=status.HTTP_204_NO_CONTENT)
