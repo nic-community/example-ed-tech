5b8ac2dde1cd8b7dd4ce3705f9453b3348a3ebb5
D-Diyazzz <diyazzz.dev@gmail.com>
2023-02-23 00:35:32 +0600

    Change urls in homework

--- homework/urls.py
+++ homework/urls.py
@@ -9,9 +9,9 @@ from .views.grade_views import *
 
 
 router = routers.SimpleRouter()
-router.register(r'tasks', TaskViewSet, basename="tasksviewset")
-router.register(r'answers', AnswerViewSet, basename="answersviewset")
-router.register(r'grades', GradeViewSet, basename="gradesviewset")
+router.register(r'/tasks', TaskViewSet, basename="tasksviewset")
+router.register(r'/answers', AnswerViewSet, basename="answersviewset")
+router.register(r'/grades', GradeViewSet, basename="gradesviewset")
 
 
 urlpatterns = [
